a91ca5ce57d21f3b38ad0f2dd150837c9a8f6ca7
ksy13001 <ksy13001@gmail.com>
2021-08-12 00:26:03 +0900

    빌더패턴 추가

--- src/main/java/com/web02/web/dto/PostsListResponseDto.java
+++ src/main/java/com/web02/web/dto/PostsListResponseDto.java
@@ -1,21 +1,39 @@
 package com.web02.web.dto;
 
 import com.web02.domain.posts.Posts;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+
 @Getter
+@NoArgsConstructor
 public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
     private LocalDateTime modifiedDate;
+    private Long count;
+
+    @Builder
+    public PostsListResponseDto(Long id, String title, String author,LocalDateTime modifiedDate, Long count ){
+        this.id=id;
+        this.title=title;
+        this.author=author;
+        this.modifiedDate=modifiedDate;
+        this.count=count;
+    }
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
         this.modifiedDate = entity.getModifiedDate();
+        this.count=entity.getCount();
     }
+
+
 }
