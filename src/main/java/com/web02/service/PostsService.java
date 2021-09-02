060f6a59b6e8631feee9bfd83207fad37aa8071d
ksy13001 <ksy13001@gmail.com>
2021-09-02 16:07:26 +0900

    검색

--- src/main/java/com/web02/service/PostsService.java
+++ src/main/java/com/web02/service/PostsService.java
@@ -9,9 +9,13 @@ import com.web02.web.dto.PostsSaveRequestDto;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +24,8 @@ import java.util.stream.Collectors;
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+    private static final int PAGE_COUNT=4;  // 페이지 개수
+    private static final int PAGE_MAX_POSTS=4;  //한페이지 안의 게시글 개수
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
@@ -51,7 +57,7 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
-    @Transactional(readOnly = true) //readOnly >> 트랜잭션 범위를 유지하며 조회 속도 개선// 등록, 수정, 삭제 기능 없는 메소드에사용
+    @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc() {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new) //= map(posts-> new PostsListResponseDto(posts))
@@ -62,6 +68,7 @@ public class PostsService {
     public List<PostsListResponseDto> searchPosts(String keyword){
         List<Posts> posts = postsRepository.findByTitleContaining(keyword);
         List<PostsListResponseDto> postsListResponseDto =new ArrayList<>();
+
 
         if(posts.isEmpty()){    //검색결과 없음
             return postsListResponseDto;
@@ -79,5 +86,41 @@ public class PostsService {
                 .author(post.getAuthor())
                 .count(post.getCount())
                 .build();
+    }
+
+    public List<PostsListResponseDto> getPostList(Integer pageNumber){
+
+        Page<Posts> page = postsRepository.findAll(PageRequest.of(pageNumber-1,PAGE_MAX_POSTS, Sort.by(Sort.Direction.ASC,"createdDate")));
+
+        List<Posts> posts=page.getContent();
+        List<PostsListResponseDto> postsListResponseDto=new ArrayList<>();
+
+        for(Posts post: posts){
+            postsListResponseDto.add(this.toDto(post));
+        }
+        return postsListResponseDto;
+    }
+
+    @Transactional
+    public Long getPostCount() {
+        return postsRepository.count();
+    }
+
+    public Integer[] getPageList(Integer curPageNum) {
+        Integer[] pageList = new Integer[PAGE_COUNT];
+        Double postsTotalCount = Double.valueOf(this.getPostCount());   //총 게시물 개수
+        Integer totalLastPageNum = (int)(Math.ceil((postsTotalCount/PAGE_MAX_POSTS))); //총 페이지 개수
+
+        Integer blockLastPageNum = (totalLastPageNum > curPageNum + PAGE_COUNT) //마지막 페이지 넘버
+                ? curPageNum + PAGE_COUNT
+                : totalLastPageNum;
+
+        curPageNum = (curPageNum <= 3) ? 1 : curPageNum - 2;    //처음 시작 페이지
+
+        for (int val = curPageNum, idx = 0; val <= blockLastPageNum; val++, idx++) {
+            pageList[idx] = val;
+        }   //페이지 번호 지정
+
+        return pageList;
     }
 }
