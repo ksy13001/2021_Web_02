190cd5af1bc95698c0fa30e72e8828c8a9857977
ksy13001 <ksy13001@gmail.com>
2021-09-05 15:26:43 +0900

    댓글등록

--- src/main/java/com/web02/web/CommentsController.java
+++ src/main/java/com/web02/web/CommentsController.java
@@ -4,7 +4,9 @@ package com.web02.web;
 import com.web02.service.CommentsService;
 import com.web02.service.PostsService;
 import com.web02.web.dto.CommentsRequestDto;
+import com.web02.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -14,15 +16,20 @@ public class CommentsController {
     private final PostsService postService;
     private final CommentsService commentsService;
 
-    @PostMapping("/api/v1/comments/{posts_id}")
-    public Long save(@RequestBody CommentsRequestDto commentsDto){
-         return commentsService.saveComments(commentsDto);
+
+    @PostMapping("comments/{postId}")
+    public Long saveComments(@PathVariable Long postId, @RequestBody CommentsRequestDto commentsRequestDto){
+        commentsRequestDto.setPostNo(postId);
+        return commentsService.saveComments(commentsRequestDto,postId);
     }
 
-    @DeleteMapping("/api/v1/comments/{id}")
-    public Long delete(@PathVariable Long id) {
-        commentsService.delete(id);
+
+
+    @DeleteMapping("/comments/{postId}/{id}")
+    public Long deleteComments(@PathVariable Long postId,@PathVariable Long id){
+        commentsService.deleteComments(id);
         return id;
     }
+
 }
 
