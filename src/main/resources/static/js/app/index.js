7bc3753bc0981fb0870061f606d7cfffcc109127
ksy13001 <ksy13001@gmail.com>
2021-09-07 17:27:43 +0900

    js

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -2,6 +2,7 @@
 var main = {    //다른 js랑 겹치지 않게
     init : function () {
         var _this = this;
+        var comment=this;
         $('#btn-save').on('click', function () {    //btn-save: id 엘리먼트 클릭시 save 함수 실행
             _this.save();
         });
@@ -14,17 +15,19 @@ var main = {    //다른 js랑 겹치지 않게
             _this.delete();
         });
 
-         $('#btn-saveComment').on('click', function () {  //삭제 함수 실행
+        $('#btn-saveComment').on('click', function () {
              _this.saveComment();
-         });
+        });
 
+        $('#btn-deleteComment').on('click', function () {
+            _this.deleteComment();
+        });
     },
     save : function () {
         var data = {
             title: $('#title').val(),   //jquery 객체 title 값 불러오기
             author: $('#author').val(),
-            content: $('#content').val(),
-
+            content: $('#content').val()
         };
 
         $.ajax({
@@ -44,7 +47,6 @@ var main = {    //다른 js랑 겹치지 않게
         var data = {
             title: $('#title').val(),
             content: $('#content').val()
-
         };
 
         var id = $('#id').val();
@@ -74,30 +76,30 @@ var main = {    //다른 js랑 겹치지 않게
             alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
-            alert(JSON.stringify(error));
+            alert('댓글이 있는 게시물은 삭제할 수 없습니다.');
         });
     },
+
     saveComment:function(){
-        var data = {
-            comments_author:$('#comment_author').val(),
-            comments_content:$('#comment_content').val()
-                    };
-        var id = $('#id').val();
+         var data = {
+               author:$('#comment_author').val(),
+               content:$('#comment_content').val()
+                        };
+         var postId = $('#id').val();
 
-         $.ajax({
-                    type: 'POST',
-                    url: '/api/v1/comments',
-                    dataType: 'json',
-                    contentType:'application/json; charset=utf-8',
-                    data: JSON.stringify(data)
-                }).done(function() {
-                    alert('댓글이 등록되었습니다.');
-                    window.location.href = '/';
-                }).fail(function (error) {
-                    alert(JSON.stringify(error));
-                });
-    }
-
+             $.ajax({
+                        type: 'POST',
+                        url: '/comments/'+postId,
+                        dataType: 'json',
+                        contentType:'application/json; charset=utf-8',
+                        data: JSON.stringify(data)
+                    }).done(function() {
+                        alert('댓글이 등록되었습니다.');
+                        window.location.href = '/posts/'+postId;
+                    }).fail(function (error) {
+                        alert(JSON.stringify(error));
+                    });
+        }
 };
-
 main.init();
+
