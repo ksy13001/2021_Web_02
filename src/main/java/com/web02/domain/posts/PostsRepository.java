f15a8e4a6fb0238d1c4b4ca26ee2992a86f68779
ksy13001 <ksy13001@gmail.com>
2021-09-07 17:46:42 +0900

    updateViews

--- src/main/java/com/web02/domain/posts/PostsRepository.java
+++ src/main/java/com/web02/domain/posts/PostsRepository.java
@@ -1,9 +1,13 @@
 // Entity 클래스, 기본 Entity Repository는 함께 위치해야함, 도메인 페이지에서 관리
 package com.web02.domain.posts;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,4 +17,10 @@ public interface PostsRepository extends JpaRepository<Posts,Long> {
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
     List<Posts> findByTitleContaining(String keyword);
+
+    @Modifying
+    @Query("update Posts p set p.views = p.views + 1 where p.id = :id")
+    int updateViews(@Param("id") Long id);
+
+
 }
